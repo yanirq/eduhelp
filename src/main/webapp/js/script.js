f1754b143621601aa5c561a34d409cc25f5dc4b9
hagaigreenfeld <hagaigreenfeld@gmail.com>
2018-03-22 01:56:44 +0200

    changes in homepage: 1. my events upcoming events script.js - load events from events.json

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,28 +1,60 @@
-function getEventData(id) {
-    var ResultHtml='';
-    var resultsContainer = document.getElementById('eventCard');
-    var productsCount = 0;
-    $.ajax({
-        url: endpoint + imageUrl,
-        type: 'GET',
-        contentType: 'application/json',
-        dataType: 'json',
-        proccessData: false,
-        success: function(data) {
 
-            var haveItems = false;
-            console.log(data);
-            if(data.hasOwnProperty('results')){
-                //console.log(data.results);
-                imageResultHtml = '<img src="' + imageUrl + '" ><br><p>' + data.results +'</p>';
-            }
-            resultsContainer.innerHTML = imageResultHtml;
-        },
-        error: function(error) {
-            imageResultHtml = 'INVALID INPUT, CHECK LOG';
-            console.log(error);
-            resultsContainer.innerHTML = imageResultHtml;
+var li = "<div class='timeline-badge'><i class='fa fa-check'></i></div><div class='timeline-panel'><div class='row1'>";
+var div = "<div class='col-lg-5 col-md-6'><div class='panel panel-primary'><div class='row1'>";
+var preLi = "<li>" + li;
+var preLiInvert = "<li class='timeline-inverted'>" + li;
+var buttons = "<div class='eventClicks'><button type='button' class='btn btn-warning'>להרשמה</button><button type='button' class='btn btn-info'>לפרטים</button></div>";
+var invert=true;
+var html="";
+var userId="1";
+(fillData)
+();
+function fillData(){
+    var jsonData = "events.json";
+    $.getJSON(jsonData, {
+        format: "json"
+    })
+        .done(myData,upcomingEvents);
+
+}
+function upcomingEvents(data) {
+    html="";
+    $.each(data.events, function (i, event) {
+        console.log("upcoming:"+event.name);
+        if (invert) {
+            html += preLi;
+            invert=false;
+        } else {
+            html += preLiInvert;
+            invert=true;
+        }
+        html += "<h3>" + event.name + "</h3></div>" + "<div class='row2'><img src=" + event.image + "><div class='eventData'><p class='eventDetails'>" + event.creator + "</p><p class='eventDetails'>" + event.date + "</p><p class='eventDetails'>" + event.location + "</p></div>";
+        html += "<div class='eventClicks'><button type='button' class='btn btn-warning'>להרשמה</button><a href='events.html?eventId=" + event.id + "' class='btn btn-info'>לפרטים</a></div></div>";
+        html += "<div class='row3'>";
+        $.each(event.skills, function(i,skill) {
+            html += "<button type='button' class='btn btn-primary'>" + skill + "</button>";
+        });
+        html+="</div></div>";
+    });
+    //console.log(html);
+    $(html).appendTo("#timeline");
+}
+function myData(data){
+    html="";
+    $.each(data.events, function (i, event) {
+        console.log("my: "+event.name);
+        if (event.users.indexOf("2") > -1) {
+            html += div;
+
+            html += "<h3>" + event.name + "</h3></div>" + "<div class='row2'><img src=" + event.image + "><div class='eventData'><p class='eventDetails'>" + event.creator + "</p><p class='eventDetails'>" + event.date + "</p><p class='eventDetails'>" + event.location + "</p></div>";
+            html += "<div class='eventClicks'><button type='button' class='btn btn-warning'>להרשמה</button><a href='events.html?eventId=" + event.id + "' class='btn btn-info'>לפרטים</a></div></div>";
+            html += "<div class='row3'>";
+            $.each(event.skills, function (i, skill) {
+                html += "<button type='button' class='btn btn-primary'>" + skill + "</button>";
+            });
+            html += "</div></div></div>";
         }
     });
-
+    //console.log(html);
+    $(html).appendTo("#eventCard");
 }
